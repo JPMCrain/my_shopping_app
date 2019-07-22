6d63cf58e901cbdbe9f697927992ca5db8839ac6
Jean-Paul-Michael Crain <jpmcrain@gmail.com>
2019-07-22 03:27:20 -0900

    header component set up and styled

---
+++ my_shopping_app/src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+import styles from './index.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartArrowDown } from '@fortawesome/free-solid-svg-icons'
+
+class Header extends Component {
+	render() {
+		return (
+			<div>
+				<header className={styles.headerWrapper}>
+					<div className={styles.heading}>
+						<h1>My Shopping App</h1>
+					</div>
+					<div className={styles.headingTabs}>
+						<button className={styles.goToShopTab}>Go to Shop</button>
+						<FontAwesomeIcon className={styles.fontAwesomeIcon} icon={faCartArrowDown} />
+					</div>
+				</header>
+			</div>
+		)
+	}
+}
+
+export default Header
+
+
