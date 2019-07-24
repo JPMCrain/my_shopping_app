b3a70cd6d0d3972760c0bc32552f202c08674134
Jean-Paul-Michael Crain <jpmcrain@gmail.com>
2019-07-24 06:11:56 -0900

    added Footer component updated home page

---
+++ my_shopping_app/src/components/Footer/Footer.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import styles from './index.module.css';
+
+class Footer extends Component {
+	render() {
+		return (
+			<div>
+				<footer className={styles.footerWrapper}>
+					<div className={styles.footer}>
+
+					</div>
+				</footer>
+			</div>
+		)
+	}
+}
+
+export default Footer
+
+
--- my_shopping_app/src/pages/HomePage/HomePage.jsx
+++ my_shopping_app/src/pages/HomePage/HomePage.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
+import styles from './index.module.css';
 import Header from '../../components/Header/Header.jsx';
 import Carousel from '../../components/Carousel/Carousel.jsx';
+import Footer from '../../components/Footer/Footer.jsx';
 
 class HomePage extends Component {
 	render() {
 		return (
-			<div>
+			<div className={styles.Wrapper}>
 				<Header />
 				<Carousel />
+				<Footer />
 			</div>
 		)
 	}
