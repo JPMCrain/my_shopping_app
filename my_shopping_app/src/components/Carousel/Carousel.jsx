dc4b2af8e53b7acb1c3cc7a5b8026c1630ac5db0
Jean-Paul-Michael Crain <jpmcrain@gmail.com>
2019-07-24 06:10:31 -0900

    took caurosel automation out

--- my_shopping_app/src/components/Carousel/Carousel.jsx
+++ my_shopping_app/src/components/Carousel/Carousel.jsx
@@ -12,7 +12,7 @@ class Carousel extends Component {
 	constructor(props) {
 		super(props)
 		this.state = {
-			images: [],
+			itemList: [],
 			currentIndex: 0,
 		}
 		this.goToNextSlide = this.goToNextSlide.bind(this);
@@ -20,11 +20,10 @@ class Carousel extends Component {
 	}
 
 	componentDidMount() {
-		this.getImages();
-		this.timer();
+		this.getItems();
 	}
 
-	getImages() {
+	getItems() {
 		let itemDataArray = jsonData;
 		let allItems = [];
 		let randomFiveItems = [];
@@ -38,23 +37,13 @@ class Carousel extends Component {
 			});
 		});
 		for (let i = 0; i < allItems.length; i++) {
-			let randomImage = allItems[Math.floor(Math.random() * allItems.length)];
+			let randomItem = allItems[Math.floor(Math.random() * allItems.length)];
 			if (randomFiveItems.length < 5) {
-				randomFiveItems.push(randomImage);
+				randomFiveItems.push(randomItem);
 			}
 		}
 		console.log(randomFiveItems)
-		this.setState({ images: randomFiveItems });
-	}
-
-	timer() {
-		setInterval(() => {
-			if (this.state.currentIndex < 4) {
-				this.setState({ currentIndex: this.state.currentIndex + 1 });
-			} else {
-				this.setState({ currentIndex: 0 })
-			}
-		}, 5000);
+		this.setState({ itemList: randomFiveItems });
 	}
 
 	goToNextSlide() {
@@ -78,16 +67,16 @@ class Carousel extends Component {
 			transform: `translateX(${this.state.activeIndex * - 100}%)`,
 			transition: '0.2s'
 		}
-		let { currentIndex, images } = this.state
+		let { currentIndex, itemList } = this.state
 		return (
 			<div className={styles.slideShow} style={sliderStyle}>
 				<ArrowLeft goToPrevSlide={this.goToPrevSlide}></ArrowLeft>
-				{images.length > 0 &&
+				{itemList.length > 0 &&
 					<Slide
-						name={images[currentIndex].name}
-						description={images[currentIndex].description}
-						price={images[currentIndex].price}
-						image={images[currentIndex].imagelink} />}
+						name={itemList[currentIndex].name}
+						description={itemList[currentIndex].description}
+						price={itemList[currentIndex].price}
+						image={itemList[currentIndex].imagelink} />}
 				<ArrowRight goToNextSlide={this.goToNextSlide}></ArrowRight>
 			</div>
 		)
