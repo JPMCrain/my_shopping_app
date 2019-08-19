b9140c40b08711135c7fe24fe7562ecb6fd77286
Jean-Paul-Michael Crain <jpmcrain@gmail.com>
2019-08-18 22:55:16 -0900

    started with shop page media screen design

--- my_shopping_app/src/components/CatogoryList/CatogoryList.jsx
+++ my_shopping_app/src/components/CatogoryList/CatogoryList.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 import styles from './index.module.css';
 
-
-
 class CatogoryList extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			isOpened: false,
+			display: 'none'
+		}
+		this.toggleCatogories = this.toggleCatogories.bind(this)
+	}
+
 
 	onClickFilter(filterKey, index) {
 		let { filters } = this.props;
@@ -12,16 +19,39 @@ class CatogoryList extends Component {
 		this.props.onFilterChange(filters);
 	}
 
+	toggleCatogories() {
+		const { display } = this.state
+		if (display === 'none') {
+			this.setState({
+				isOpened: true,
+				display: 'flex'
+			})
+		} else {
+			this.setState({
+				isOpened: false,
+				display: 'none'
+			})
+		}
+	}
+
 	render() {
 		const { catogoryLists, filters, openIndex } = this.props;
+		const style = {
+			display: this.state.display,
+		}
 		return (
-			<div className={styles.listWrapper}>
-				<ul>
+			<div>
+				<button
+					onClick={() => {
+						this.toggleCatogories()
+					}}
+					className={styles.OpenCatogories}>Open/Close</button>
+				<div className={styles.listWrapper} style={style}>
 					{
 						catogoryLists.map((catogory, catogoryIndex) => {
 							const catlistItemStyle = openIndex === catogoryIndex ? styles.catlistItemActive : styles.catlistItem
 							return (
-								<li key={catogoryIndex}>
+								<div key={catogoryIndex}>
 									<button className={catlistItemStyle}
 										onClick={(e) => {
 											this.props.onCatogoryClick(catogoryIndex)
@@ -32,37 +62,39 @@ class CatogoryList extends Component {
 
 									{
 										this.props.openIndex === catogoryIndex &&
-										<ul>
+										<div>
 											{
 												catogory.subcategories.map((item, index) => {
 
 													return (
-														<li><button
-															className={styles.sublistItem}
-															key={index}
-															onClick={(e) => {
-																if (filters.isItemViewPannelOpen) {
-																	this.onClickFilter('isItemViewPannelOpen', undefined)
-																	this.props.onCategorytItemClick(catogoryIndex, index)
-																} else {
-																	this.props.onCategorytItemClick(catogoryIndex, index)
-																}
-															}}>
-															{item.name}
-														</button>
+														<div>
+															<button
+																className={styles.sublistItem}
+																key={index}
+																onClick={(e) => {
+																	if (filters.isItemViewPannelOpen) {
+																		this.onClickFilter('isItemViewPannelOpen', undefined)
+																		this.props.onCategorytItemClick(catogoryIndex, index)
+																	} else {
+																		this.props.onCategorytItemClick(catogoryIndex, index)
+																	}
+																}}>
+																{item.name}
+															</button>
 
-														</li>
+														</div>
 													);
 												})
 											}
-										</ul>
+										</div>
 									}
-								</li>
+								</div>
 							);
 						})
 					}
-				</ul>
+				</div>
 			</div>
+
 		)
 	}
 }
